ceb54ef97cf755a4af05ea624200963ae95d0fdc
David <shaggi6e@gmail.com>
2023-05-15 12:17:26 -0500

    Agregar clase Datos Enfermedades con los metodos Agrtegarenfermedad y listarenferemdad

---
+++ DatosClinica/DatosEnfermedad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosClinica
+{
+    public class DatosEnfermedad
+    {
+        DataClasses1DataContext Db = new DataClasses1DataContext();
+        public void AgregarEnfermedad(Enfermedades enf)
+        {
+            Db.Enfermedades.InsertOnSubmit(enf);
+            Db.SubmitChanges();
+        }
+
+        public List<Enfermedades> ListarEnfermedades(int ID)
+        {
+            List<Enfermedades> lista = (from x in Db.Enfermedades where x.IdPaciente == ID select x).ToList();
+            return lista;
+        }
+    }
+}
